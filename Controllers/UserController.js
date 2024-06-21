c332941b7c88d819ed8dab3fcce40d81d39b2224
mardsonbarboza <mardson49@gmail.com>
2024-06-21 16:16:04 -0300

    funções de edit, remove, create e findall de usuarios

--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,7 +1,77 @@
 
 class UserController{
-    async FindUsers(req,res){
+    async create(req, res){
+        var {email, name} = req.body;
+        
+        if(email == undefined){
+            res.status(400);
+            res.json({err: "O e-mail é inválido!"})
+            return;
+        }
+
+        var emailExists = await User.findEmail(email);
+
+        if(emailExists){
+            res.status(406);
+            res.json({err: "O e-mail já está cadastrado!"})
+            return;
+        }
+
+        
+        await User.new(email,name);
+        
+        res.status(200);
+        res.send("Tudo OK!");
+    }
+    
+    async findUser(req, res){
+        var id = req.params.id;
+        var user =  await User.findById(id);
+
+        if(user == undefined){
+            res.status(404);
+            res.json({err: "Usuario não encontrado"})
+        }else{
+            res.status(200);
+            res.json(user); 
+        }
+    }
+
+    async edit(req,res){
+
+        var {id,name,email} = req.body;
+        var result = await User.update(id,email,name);
+
+        if(result != undefined){
+            if(result.status){
+                res.status(200);
+                res.send("Tudo ok!");
+            }else{
+                res.status(406);
+                res.send(result.err);
+            }
+        }else{
+            res.status(406);
+            res.send("ocorreu um erro no servidor")
+        }
         
     }
+
+    async remove(req,res){
+        var id = req.params.id;
+
+        var result = await User.delete(id);
+
+        if(result.status){
+            res.status(200);
+            res.send("tudo Ok!");
+        }else{
+            res.status(406);
+            res.send(result.err);
+        }
+    }
+
+    
+
 }
 module.exports = new UserController
